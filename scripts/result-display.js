77fb338e9520e6c6c4514ddb1dbf48c996a021fd
Andres Gomez <andresgomezjr89@gmail.com>
2020-04-25 07:11:49 -0400

    fixed links

--- scripts/result-display.js
+++ scripts/result-display.js
@@ -15,6 +15,15 @@ function assertFormatting (htmlString, cssString, jsString) {
     // assert it's surrounded by an <html> tag
     htmlNode.innerHTML = trim.startsWith('<html>') ? trim.substring(6, trim.length - 7) : trim;
 
+    // fix links to always open in a new tab,
+    // links inside iframes generate CORS errors when going to a differnt domain
+    // TODO: waht if user wants link to open in editor?
+    let links = htmlNode.getElementsByTagName('a');
+    for (let i = 0; i < links.length; i++)
+        links[i].setAttribute('target', '_base');
+
+
+
     // reconstruct html as string
     let head = `<head>${htmlNode.getElementsByTagName('head')[0].innerHTML}<style>${cssString}</style></head>`;
     let body = `<body>${htmlNode.getElementsByTagName('body')[0].innerHTML}<script>${jsString}</script></body>`;
